b5d365a69c0978382673961c344e44eb4be25d2c
limiaomiao <155894083+1imiaomiao@users.noreply.github.com>
2024-04-03 18:18:03 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <router-view />
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import { initSocket, useSocketServer } from "@/hooks/useSocketHooks";
+
+const { listeningAllMessage } = useSocketServer();
+
+onMounted(() => {
+  console.log("项目 init....");
+  initSocket();
+  listeningAllMessage();
+});
+</script>
 
 <style></style>
